866aa47788cc497e7eaf1ef595dd04ba0a0e269b
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-14 20:48:40 +0600

    Add code TASK001

--- TASK001/Program.cs
+++ TASK001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число: ");
+int value = int.Parse(Console.ReadLine());
+int b = value / 10 % 10;
+Console.WriteLine(b);
