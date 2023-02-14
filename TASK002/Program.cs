2ca6f1424e8cea6106bc7538f8b6d3ff81479425
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-14 20:58:49 +0600

    Add code TASK002

--- TASK002/Program.cs
+++ TASK002/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+//
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int value = int.Parse(Console.ReadLine());
+int b = value % 10;
+Console.WriteLine(b);
