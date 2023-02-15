4c741b8aee47ce19475b933b79982f6e1122af74
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-15 22:15:45 +0600

    Add code TASK002

--- TASK002/Program.cs
+++ TASK002/Program.cs
@@ -5,7 +5,29 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+//Console.WriteLine("Введите число: ");
+//int number = int.Parse(Console.ReadLine());
+//int thirdnumber = number % 10;
+//Console.WriteLine(b);
+
+int thirdnumber(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result;
+}
+
 Console.WriteLine("Введите число: ");
-int value = int.Parse(Console.ReadLine());
-int b = value % 10;
-Console.WriteLine(b);
+int number = int.Parse(Console.ReadLine());
+
+if (thirdnumber(number) == -1)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine("Третья цифра: ", thirdnumber(number));
